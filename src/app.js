e6636dfcdacfe1ef959a05d416dcb9aee31bc905
Matthieu Mota <matthieumota@gmail.com>
2020-05-26 14:38:05 +0200

    Help calc TP

--- src/app.js
+++ src/app.js
@@ -19,3 +19,21 @@ let car = new Car('red');
 car.color = 'blue';
 
 console.log(car.color);
+
+/**
+ * TP Calculatrice
+ */
+
+import Calculator from './Calculator';
+let calculator = new Calculator();
+
+// 1ère étape : Déclarer un événement au clic sur les button
+// querySelector, getElementsBy... addEventListener, onclick
+// 2ème étape : Récupérer la valeur du button cliqué
+// 3ème étape : Modifier un objet existant dans Calculator avec
+// l'événement du clic
+// 4ème étape : Distinguer les opérateurs des chiffres.
+// Si c'est un chiffre, on l'ajoute au chiffre précédent pour faire
+// un nombre sino, on stocke l'opérateur.
+
+calculator.init();
